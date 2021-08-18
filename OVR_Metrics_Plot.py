9412b66754439fb0432b32a3eb8a3e485f7ed258
Colin Hallett <colinhallett@gmail.com>
2021-08-18 11:57:56 +0100

    changeable data

--- OVR_Metrics_Plot.py
+++ OVR_Metrics_Plot.py
@@ -4,33 +4,33 @@ import analyser as ana
 import colors
 from analyser import MetricData
 
-"file browser" \
-"processed csvs" \
-"selection of csvs" \
-"show plot"
-
 metrics: list[MetricData] = []
+selected_metrics: list[str] = []
 
 def import_csvs(csvs: str):
     selections = csvs.split(";")
     for csv in selections:
         if not any(x.name == csv for x in metrics):
             metrics.append(ana.process_csv(csv))
-    window['metrics_list'].update(values=metrics)
+    window['imported_metrics'].update(values=metrics)
+    window['selected_metrics'].update(values=metrics[0].data_frame.head())
 
 
 def draw_plot():
-    data = window['metrics_list'].get()
-    ana.plot_metrics(data, colors.COLORS)
+    data = window['imported_metrics'].get()
+    selected_data = window['selected_metrics'].get()
+    ana.plot_metrics(data, selected_data, colors.COLORS)
     plt.show(block=False)
 
 
 layout = [[sg.Input(key='_FILEBROWSE_', enable_events=True, visible=False)], [sg.FilesBrowse("Process", target='_FILEBROWSE_'), sg.Text("Process OVR Metrics CSVs")],
           [sg.Button('Plot'), sg.Text("Plot selected metrics")],
           [sg.Text("Select metrics:")],
-          [sg.Listbox(metrics, key='metrics_list', size=(300,500), select_mode=sg.LISTBOX_SELECT_MODE_MULTIPLE, highlight_background_color="green")]]
+          [sg.Listbox(metrics, key='imported_metrics', size=(20, 20), select_mode=sg.LISTBOX_SELECT_MODE_MULTIPLE),
+           sg.Listbox(selected_metrics, key='selected_metrics', size=(20, 20), select_mode=sg.LISTBOX_SELECT_MODE_MULTIPLE)]
+          ]
 
-window = sg.Window('OVRMetrics Plot', layout, size=(300,500))
+window = sg.Window('OVRMetrics Plot', layout)
 while True:
     event, values = window.read()
     if event == sg.WIN_CLOSED:
--- analyser.py
+++ analyser.py
@@ -5,20 +5,11 @@ import numpy as np
 from scipy import interpolate
 import pandas as pd
 
-#
-# Rows:
-# 0 = TimeStamp
-# 1 = avaliable_MB
-# 10 = cpu_level
-# 11 = gpu_level
-# 17 = average_frame_rate
-# 30 = cpu_utilisation
-# 39 = gpu_utilisation
-# 41 = app_rss_MB
 
 class MetricData:
     def __init__(self, path: str):
         self.data_frame: pd.DataFrame = pd.read_csv(path)
+        self.data_frame = self.data_frame.loc[:, (self.data_frame != 0).any(axis=0)]
         self.name = path
 
     def __str__(self):
@@ -30,130 +21,44 @@ def process_csv(path: str) -> MetricData:
     return MetricData(path)
 
 
-def plot(y_values: [], time_stamps: [], axis, ylabel: str, ylim: [], smooth: bool, colors: list[str]):
+def plot(y_values: [], time_stamps: [], axis, ylabel: str, smooth: bool, colors: list[str]):
     ax_x = axis
-    ax_x.set_ylim(ylim)
+    minimum = min(min(x) for x in y_values)
+    maximum = max(max(x) for x in y_values)
+    ax_x.set_ylim([minimum * 0.75, maximum * 1.25])
     ax_x.set_ylabel(ylabel, loc='center')
 
     for iteration, y in enumerate(y_values):
         ax = ax_x.twinx()
-        ax.set_ylim(ylim)
-        xnew = time_stamps[iteration]
+        ax.set_ylim([minimum * 0.75, maximum * 1.25])
+        x_new = time_stamps[iteration]
         new_y = y
         if smooth:
-            xnew = np.linspace(0, int(time_stamps[iteration][len(time_stamps[iteration]) - 1]), 40)
+            x_new = np.linspace(0, int(time_stamps[iteration][len(time_stamps[iteration]) - 1]), 40)
             bspline = interpolate.make_interp_spline(time_stamps[iteration], new_y)
-            new_y = bspline(xnew)
+            new_y = bspline(x_new)
 
         color = "black"
         if len(colors) > iteration:
             color = colors[iteration]
-        ax.plot(xnew, new_y, color=color)
+        ax.plot(x_new, new_y, color=color)
         ax.get_yaxis().set_visible(False)
 
 
-def plot_metrics(metric_data: list[MetricData], colors: list[str]):
+def plot_metrics(metric_data: list[MetricData], selected_data: list[str], colors: list[str], smooth = False):
     lines = [(mlines.Line2D([], [], color=colors[iteration], label=str(x))) for iteration, x in enumerate(metric_data)]
 
-    fig, axes = plt.subplots(4)
+    axes_count = len(selected_data)
+    fig, axes = plt.subplots(axes_count)
 
-    available_mb = [x.data_frame["available_memory_MB"] for x in metric_data]
-    cpu_uti = [x.data_frame["cpu_utilization_percentage"] for x in metric_data]
-    gpu_uti = [x.data_frame["gpu_utilization_percentage"] for x in metric_data]
-    fps = [x.data_frame["average_frame_rate"] for x in metric_data]
-    time_stamps = [x.data_frame["Time Stamp"] for x in metric_data]
+    if axes_count > 1:
+        for it, s in enumerate(selected_data):
+            plot([x.data_frame[s] for x in metric_data], [x.data_frame["Time Stamp"] for x in metric_data], axes[it], s, smooth, colors)
+    else:
+        plot([x.data_frame[selected_data[0]] for x in metric_data], [x.data_frame["Time Stamp"] for x in metric_data], axes, selected_data[0],
+             smooth, colors)
 
-    plot(available_mb, time_stamps, axes[0], "AMB", [1000, 4000], False,colors)
-
-    plot(cpu_uti, time_stamps, axes[1], "CPU Utilisation", [0, 110], True,colors)
-    plot(fps, time_stamps, axes[2], "FPS", [0, 130], False,colors)
-    plot(gpu_uti, time_stamps, axes[3], "GPU Utilisation", [0, 110], False,colors)
     plt.legend(handles=lines, loc='center left', bbox_to_anchor=(1, 0.5), ncol=1, fancybox=True, shadow=True)
     fig.subplots_adjust(left=0.05, right=0.9, top=0.95, bottom=0.05)
     mng = plt.get_current_fig_manager()
     mng.window.state('zoomed')
-
-
-# def print_averages(results: list[MetricData]):
-#     mb = 0.
-#     cpu = 0.
-#     gpu = 0.
-#     fps = 0.
-#     for m in results:
-#         mb += sum(m.available_mb) / len(m.available_mb)
-#         cpu += sum(m.cpu_utilisation) / len(m.cpu_utilisation)
-#         gpu += sum(m.gpu_utilisation) / len(m.gpu_utilisation)
-#         fps += sum(m.ave_fps) / len(m.ave_fps)
-#
-#     mb /= len(results)
-#     cpu /= len(results)
-#     gpu /= len(results)
-#     fps /= len(results)
-#     print(round(mb, 2))
-#     print(round(cpu, 2))
-#     print(round(gpu, 2))
-#     print(round(fps, 2))
-#
-# vul_multiview_1 = process_csv("OVR_Metrics/autoplay/vulkan_multiview/vmv_1.csv")
-# vul_multiview_2 = process_csv("OVR_Metrics/autoplay/vulkan_multiview/vmv_2.csv")
-# vul_multiview_3 = process_csv("OVR_Metrics/autoplay/vulkan_multiview/vmv_3.csv")
-# vul_multiview_4 = process_csv("OVR_Metrics/autoplay/vulkan_multiview/vmv_4.csv")
-# vul_multiview_5 = process_csv("OVR_Metrics/autoplay/vulkan_multiview/vmv_5.csv")
-# vul_multiview_6 = process_csv("OVR_Metrics/autoplay/vulkan_multiview/vmv_6.csv")
-# vul_multiview_7 = process_csv("OVR_Metrics/autoplay/vulkan_multiview/vmv_7.csv")
-# vul_multiview_8 = process_csv("OVR_Metrics/autoplay/vulkan_multiview/vmv_8.csv")
-# vul_multiview_9 = process_csv("OVR_Metrics/autoplay/vulkan_multiview/vmv_9.csv")
-# vul_multiview = [vul_multiview_1, vul_multiview_2, vul_multiview_3,vul_multiview_4,vul_multiview_5,vul_multiview_6,vul_multiview_7,vul_multiview_8, vul_multiview_9 ]
-#
-# vul_multipass_1 = process_csv("OVR_Metrics/autoplay/vulkan_multipass/vmp_1.csv")
-# vul_multipass_2 = process_csv("OVR_Metrics/autoplay/vulkan_multipass/vmp_2.csv")
-# vul_multipass_3 = process_csv("OVR_Metrics/autoplay/vulkan_multipass/vmp_3.csv")
-# vul_multipass_4 = process_csv("OVR_Metrics/autoplay/vulkan_multipass/vmp_4.csv")
-# vul_multipass_5 = process_csv("OVR_Metrics/autoplay/vulkan_multipass/vmp_5.csv")
-# vul_multipass_6 = process_csv("OVR_Metrics/autoplay/vulkan_multipass/vmp_6.csv")
-# vul_multipass_7 = process_csv("OVR_Metrics/autoplay/vulkan_multipass/vmp_7.csv")
-# vul_multipass_8 = process_csv("OVR_Metrics/autoplay/vulkan_multipass/vmp_8.csv")
-# vul_multipass_9 = process_csv("OVR_Metrics/autoplay/vulkan_multipass/vmp_9.csv")
-# vul_multipass = [vul_multipass_1, vul_multipass_2, vul_multipass_3,vul_multipass_4,vul_multipass_5,vul_multipass_6,vul_multipass_7,vul_multipass_8, vul_multipass_9 ]
-#
-# og_multipass_1 = process_csv("OVR_Metrics/autoplay/opengl_multipass/ogmp_1.csv")
-# og_multipass_2 = process_csv("OVR_Metrics/autoplay/opengl_multipass/ogmp_2.csv")
-# og_multipass_3 = process_csv("OVR_Metrics/autoplay/opengl_multipass/ogmp_3.csv")
-# og_multipass_4 = process_csv("OVR_Metrics/autoplay/opengl_multipass/ogmp_4.csv")
-# og_multipass_5 = process_csv("OVR_Metrics/autoplay/opengl_multipass/ogmp_5.csv")
-# og_multipass_6 = process_csv("OVR_Metrics/autoplay/opengl_multipass/ogmp_6.csv")
-# og_multipass_7 = process_csv("OVR_Metrics/autoplay/opengl_multipass/ogmp_7.csv")
-# og_multipass_8 = process_csv("OVR_Metrics/autoplay/opengl_multipass/ogmp_8.csv")
-# og_multipass_9 = process_csv("OVR_Metrics/autoplay/opengl_multipass/ogmp_9.csv")
-# og_multipass = [og_multipass_1, og_multipass_2, og_multipass_3,og_multipass_4,og_multipass_5,og_multipass_6,og_multipass_7,og_multipass_8, og_multipass_9 ]
-#
-# og_multiview_1 = process_csv("OVR_Metrics/autoplay/opengl_multiview/ogmv_1.csv")
-# og_multiview_2 = process_csv("OVR_Metrics/autoplay/opengl_multiview/ogmv_2.csv")
-# og_multiview_3 = process_csv("OVR_Metrics/autoplay/opengl_multiview/ogmv_3.csv")
-# og_multiview_4 = process_csv("OVR_Metrics/autoplay/opengl_multiview/ogmv_4.csv")
-# og_multiview_5 = process_csv("OVR_Metrics/autoplay/opengl_multiview/ogmv_5.csv")
-# og_multiview_6 = process_csv("OVR_Metrics/autoplay/opengl_multiview/ogmv_6.csv")
-# og_multiview_7 = process_csv("OVR_Metrics/autoplay/opengl_multiview/ogmv_7.csv")
-# og_multiview_8 = process_csv("OVR_Metrics/autoplay/opengl_multiview/ogmv_8.csv")
-# #og_multiview_9 = process_csv("OVR_Metrics/autoplay/opengl_multiview/ogmv_9.csv")
-# og_multiview = [og_multiview_1, og_multiview_2, og_multiview_3,og_multiview_4,og_multiview_5,og_multiview_6,og_multiview_7,og_multiview_8 ]
-#
-#
-#
-#
-#
-# print ("Vul multiview")
-# print_averages(vul_multiview)
-# print ("Vul multipass")
-# print_averages(vul_multipass)
-# print ("OG multipass")
-# print_averages(og_multipass)
-# print ("OG multiview")
-# print_averages(og_multiview)
-#
-# for i in range(8):
-#     compare = [og_multiview[i], og_multipass[i], vul_multiview[i], vul_multipass[i]]
-#     plot_metrics(compare)
-#
-# plt.show()
-#
